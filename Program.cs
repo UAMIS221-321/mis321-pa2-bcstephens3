b405bf57fc9f27db5f516e262290ecbaf5546abd
Brandon Stephens <bcstephens3@crimson.ua.edu>
2022-02-13 13:17:57 -0600

    test 1 2

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mis321_pa2_bcstephens3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
